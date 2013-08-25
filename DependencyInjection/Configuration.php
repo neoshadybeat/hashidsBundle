60ab19b40fd6026da2e6f9cc529532a25beb3852
Cayetano Soriano <neoshadybeat@gmail.com>
2013-08-25 17:55:41 +0200

    initial commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace cayetanosoriano\HashidsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('cayetanosoriano_hashids');
+        $rootNode
+            ->children()
+                ->scalarNode('salt')->defaultNull()->end()
+                ->scalarNode('min_hash_length')->defaultValue(0)->end()
+                ->scalarNode('alphabet')->defaultValue('')->end()
+                ;
+        return $treeBuilder;
+    }
+}
---
+++ cayetanosorianoHashidsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace cayetanosoriano\HashidsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class cayetanosorianoHashidsBundle extends Bundle
+{
+}
